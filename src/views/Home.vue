df132cd83c1a04efc4be4de2e69c4a7aa149e307
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-19 16:33:24 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,47 @@
 <template>
-  <div id="home"></div>
+  <div id="home" class="w-flex">
+    <div class="w-flex column no-grow">
+      <h2>Know</h2>
+      <a
+        href="https://en.wikipedia.org/wiki/Aaron_Swartz#Open_Access"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Open access</a
+      >
+      <a
+        href="https://en.wikipedia.org/wiki/Digital_preservation"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Perservation</a
+      >
+      <a
+        href="https://en.wikipedia.org/wiki/Freedom_of_information"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Information Freedom</a
+      >
+      <a
+        href="https://en.wikipedia.org/wiki/Textbook#Bundling_in_the_USA"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Bundling</a
+      >
+      <a
+        href="https://stallman.org/ebooks.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        DRM
+      </a>
+    </div>
+    <div>
+      <h1>Continue</h1>
+      <div id="recent-pdf-container"></div>
+      <h1>Important</h1>
+      <div id="continue-pdf-container"></div>
+      <div id=""></div>
+    </div>
+  </div>
 </template>
 
 <script>
