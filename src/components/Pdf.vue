92db4c9089c163f5d21fc46ebfbd41051a40c338
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-20 13:21:05 -0600

    Add component for Pdf rendering

---
+++ src/components/Pdf.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="pdf">
+    <canvas id="pdf-canvas"></canvas>
+  </div>
+</template>
+
+<script>
+import workerSrc from "!!file-loader!pdfjs-dist/build/pdf.worker.min.js";
+const pdfjsLib = require(/* webpackChunkName: "pdfjs-dist" */ `pdfjs-dist`);
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
+
+export default {
+  name: "Pdf",
+  props: {
+    pdfPath: String,
+  },
+  computed: {
+    localPdfPath() {
+      return "local-resource://" + this.pdfPath;
+    },
+  },
+  mounted() {
+    this.getPdf();
+  },
+  methods: {
+    getPdf() {
+      if (this.pdfPath.length !== 0) {
+        console.log("getPdf() called");
+        const loadingTask = pdfjsLib.getDocument(this.localPdfPath);
+
+        loadingTask.promise.then((pdf) => {
+          pdf.getPage(1).then((page) => {
+            let scale = 1;
+            let viewport = page.getViewport({ scale: scale });
+
+            let canvas = document.getElementById("pdf-canvas");
+            let context = canvas.getContext("2d");
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            const renderContext = {
+              canvasContext: context,
+              viewport: viewport,
+            };
+            page.render(renderContext);
+          });
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
