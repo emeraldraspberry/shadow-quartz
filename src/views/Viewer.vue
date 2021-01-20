cb747b26b438dfdc77ca2cd05d76adc93cbfeffb
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-20 13:22:51 -0600

    Add pdf file opening functionality

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1,10 +1,38 @@
 <template>
-  <div id="viewer"></div>
+  <div id="viewer">
+    <div id="viewer-controls" class="w-flex justify-left">
+      <w-icon xl @click="openFile">material-icons folder_open</w-icon>
+    </div>
+    <Pdf v-if="path" :pdfPath="path"></Pdf>
+  </div>
 </template>
 
 <script>
+import Pdf from "@/components/Pdf";
 export default {
   name: "Viewer",
+  components: {
+    Pdf: Pdf,
+  },
+  data() {
+    return {
+      path: "",
+    };
+  },
+  methods: {
+    openFile() {
+      console.log("openFile() call");
+      window.ipcRenderer.send("open-file");
+    },
+  },
+  mounted() {
+    window.ipcRenderer.on("return-file-path", (event, path) => {
+      this.path = path;
+    });
+  },
+  unmounted() {
+    window.ipcRenderer.removeAllListeners();
+  },
 };
 </script>
 
