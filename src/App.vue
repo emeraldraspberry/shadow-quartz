9f053de80512dc5826bdd82d5494d06b173695ca
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-23 00:34:47 -0600

    Implement welcome dialog to select and set a primary directory

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,66 @@
       <router-link to="/shadows">Shadows</router-link>
     </div>
     <router-view />
+    <MessageDialog status="welcome" v-if="isDirectoryPathEmpty && isNew">
+      <p>
+        Shadow Quartz requires a primary directory path for optimal use. This
+        directory can be used to store history, manage downloads, save
+        libraries, and cache image previews.
+      </p>
+      <br />
+      <p>The primary directory can be set later in the Settings view.</p>
+      <div class="w-flex justify-center mt4">
+        <w-button @click="selectDirectory">Select Directory</w-button>
+        <w-button @click="isNew = false" class="ml2">Close</w-button>
+      </div>
+    </MessageDialog>
   </w-app>
 </template>
+
+<script>
+import MessageDialog from "@/components/MessageDialog";
+export default {
+  name: "App",
+  components: {
+    MessageDialog: MessageDialog,
+  },
+  mounted() {
+    window.ipcRenderer.send("load-settings");
+    window.ipcRenderer.on("load-settings-reply", (event, args) => {
+      this.settings = args.settings;
+    });
+    window.ipcRenderer.on("select-directory-reply", (event, args) => {
+      if (args) {
+        this.isNew = false;
+        console.log("Directory path successfully stored.");
+      }
+    });
+  },
+  unmounted() {
+    window.ipcRenderer.removeAllListeners();
+  },
+  computed: {
+    isDirectoryPathEmpty() {
+      if (this.settings !== null && this.settings.library.path.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  data() {
+    return {
+      settings: null,
+      isNew: true,
+    };
+  },
+  methods: {
+    selectDirectory() {
+      window.ipcRenderer.send("select-directory");
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @font-face {
