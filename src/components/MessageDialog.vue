4b0fe18b950838095d6fe1fe5201a41700fc184e
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-22 21:55:15 -0600

    Add MessageDialog component

---
+++ src/components/MessageDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="message-dialog">
+    <w-dialog
+      v-if="status.includes('info')"
+      content-class="grey-dark5--bg"
+      title-class="info-dark3--bg"
+      transition="bounce"
+    >
+      <template #title>
+        <w-icon xl class="mr2">material-icons info</w-icon>
+        Information
+      </template>
+      <slot></slot>
+    </w-dialog>
+    <w-dialog
+      v-else-if="status.includes('success')"
+      content-class="grey-dark5--bg"
+      title-class="success-dark3--bg"
+      transition="bounce"
+    >
+      <template #title>
+        <w-icon xl class="mr2">material-icons check_circle</w-icon>
+        Success
+      </template>
+      <slot></slot>
+    </w-dialog>
+    <w-dialog
+      v-else-if="status.includes('warning')"
+      content-class="grey-dark5--bg"
+      title-class="warning-dark3--bg"
+      transition="bounce"
+    >
+      <template #title>
+        <w-icon xl class="mr2">material-icons warning</w-icon>
+        Warning
+      </template>
+      <slot></slot>
+    </w-dialog>
+    <w-dialog
+      v-else-if="status.includes('error')"
+      content-class="grey-dark5--bg"
+      title-class="error-dark3--bg"
+      transition="bounce"
+    >
+      <template #title>
+        <w-icon xl class="mr2">material-icons error</w-icon>
+        Error
+      </template>
+      <slot></slot>
+    </w-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessageDialog",
+  props: {
+    status: {
+      type: String,
+      default: "info",
+      validator: (value) => {
+        // Must match a string
+        return ["success", "error", "warning", "info"].indexOf(value) !== -1;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
