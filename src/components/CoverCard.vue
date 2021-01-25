52d439a8d725fc9b5f5150af108acd434790b6c0
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-25 13:46:44 -0600

    Use model to import information on PDF

--- src/components/CoverCard.vue
+++ src/components/CoverCard.vue
@@ -1,45 +1,84 @@
 <template>
-  <div id="cover-card" class="ma2 w-flex column justify-end no-grow">
+  <div id="cover-card" class="ma2">
+    <w-image
+      id="cover-image"
+      tag="img"
+      :src="imageData"
+      width="200"
+      @click="routeLink"
+    >
+    </w-image>
     <div id="cover-information">
       <p>{{ title }}</p>
-      <p>{{ pages }}</p>
+      <p>{{ author }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import PdfData from "@/model/PdfData";
+
 export default {
   name: "CoverCard",
   props: {
-    title: {
-      type: String,
-      required: true,
-      default: "Document",
-    },
-    fileName: {
-      type: String,
-      required: true,
-      default: "Filename",
-    },
-    fileType: {
-      type: String,
-      required: true,
-      default: "pdf",
-    },
-    pages: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    currentPage: {
-      type: Number,
-      required: false,
-      default: 1,
-    },
     path: {
       type: String,
       required: true,
       default: "",
+    },
+  },
+  mounted() {
+    this.Data.metaData.then((data) => {
+      this.metaData = data;
+      console.log(this.metaData.info.Author, this.metaData.info.Title);
+    });
+
+    this.Data.imageUrl.then((data) => {
+      this.imageData = data;
+    });
+  },
+  unmounted() {
+    delete this.Data;
+  },
+  data() {
+    return {
+      Data: new PdfData(this.path),
+      imageData: null,
+      metaData: null,
+    };
+  },
+  computed: {
+    author() {
+      if (
+        this.metaData !== null &&
+        this.metaData !== undefined &&
+        this.metaData.Author !== "" &&
+        this.metaData.Author !== undefined
+      ) {
+        return this.metaData.info.Author;
+      } else {
+        return "";
+      }
+    },
+    title() {
+      if (
+        this.metaData !== null &&
+        this.metaData !== undefined &&
+        this.metaData.info.Title !== "" &&
+        this.metaData.info.Title !== undefined
+      ) {
+        return this.metaData.info.Title;
+      } else {
+        return this.path;
+      }
+    },
+  },
+  methods: {
+    routeLink() {
+      this.$router.push({
+        name: "Viewer.get",
+        params: { referredPath: this.path },
+      });
     },
   },
 };
@@ -47,12 +86,12 @@ export default {
 
 <style lang="scss" scoped>
 #cover-card {
-  background-color: darkslategrey;
-  width: 240px;
-  height: 320px;
+  position: relative;
+  height: min-content;
+  max-width: 240px;
 }
 
-#cover-information {
-  background-color: rgba($base-bg-color, 0.8);
+#cover-image {
+  width: 100%;
 }
 </style>
